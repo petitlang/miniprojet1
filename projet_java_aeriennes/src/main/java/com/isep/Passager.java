a8ce13943c9ef0aeecb091130278661c905e21e9
petitlang <L2164753957@gmail.com>
2024-11-29 12:17:50 +0100

    motifier les methodes de la classe passager et reservation

--- projet_java_aeriennes/src/main/java/com/isep/Passager.java
+++ projet_java_aeriennes/src/main/java/com/isep/Passager.java
@@ -1,11 +1,15 @@
 package com.isep;
 
+import java.util.ArrayList;
+
 public class Passager extends Personne{
     private String Passeport;
+    protected ArrayList<Reservation> reservations;
 
     public Passager(String Identifiant, String Nom, String Adresse, String Contact, String Passeport) {
         super(Identifiant, Nom, Adresse, Contact);
         this.Passeport = Passeport;
+        this.reservations = new ArrayList<Reservation>();
     }
 
     public String getPasseport() {
@@ -16,15 +20,28 @@ public class Passager extends Personne{
         this.Passeport = Passeport;
     }
 
-    public void reserverVol(int numeroVol, String numeroReservation){
-        System.out.println("Vol réservé pour le passager : " + this.getNom());
+    public void reserverVol(int numeroVol, int numeroReservation, String date, String statut) {
+        Reservation newReservation = new Reservation(numeroReservation, date, statut, this);
+        newReservation.addVol(numeroVol);
+        this.reservations.add(newReservation);
+        System.out.println("Vol " + numeroVol + " réservé avec succès pour " + this.getNom());
     }
 
-    public void annulerReservation(){
-        System.out.println("Reservation annulée pour le passager : " + this.getNom());
+    public void annulerReservation(int  numeroReservation) {
+        for (Reservation reservation : reservations) {
+            if (reservation.getNumeroReservation() == numeroReservation) {
+                reservations.remove(reservation);
+                System.out.println("Reservation " + numeroReservation + " annulée.");
+            }else{
+                System.out.println("Aucune réservation trouvée pour le numéro " + numeroReservation);
+            }
+        }
+        
     }
 
-    public void obtenirReservation(){
-        System.out.println("Reservation obtenue par le passager : " + this.getNom());
+    public void obtenirReservations() {
+        for (Reservation reservation : reservations) {
+            System.out.println("Réservation : " + reservation.getNumeroReservation() + ", Date : " + reservation.getDateReservation());
+        }
     }
 }
--- projet_java_aeriennes/src/main/java/com/isep/Reservation.java
+++ projet_java_aeriennes/src/main/java/com/isep/Reservation.java
@@ -3,22 +3,25 @@ package com.isep;
 import java.util.ArrayList;
 
 public class Reservation{
-    private String numeroReservation;
+    private int numeroReservation;
     private String dateReservation;
     private String statut;
     protected ArrayList<Vol> volsReserve;
+    protected Passager passager;
 
-    public Reservation(String numeroReservation, String dateReservation, String statut) {
+    public Reservation(int numeroReservation, String dateReservation, String statut, Passager passager) {
         this.numeroReservation = numeroReservation;
         this.dateReservation = dateReservation;
         this.statut = statut;
+        this.volsReserve = new ArrayList<Vol>();
+        this.passager = passager;
     }
 
-    public String getNumeroReservation() {
+    public int getNumeroReservation() {
         return numeroReservation;
     }
 
-    public void setNumeroReservation(String numeroReservation) {
+    public void setNumeroReservation(int numeroReservation) {
         this.numeroReservation = numeroReservation;
     }
 
@@ -38,16 +41,56 @@ public class Reservation{
         this.statut = statut;
     }
 
-    public void confirmerReservation(){
-        System.out.println("Reservation confirmée pour le passager : " + this.getNumeroReservation());
+    public void confirmerReservation(int numeroReservation){
+        for(Reservation reservation : passager.reservations){
+            if(reservation.getNumeroReservation() == numeroReservation){
+                reservation.setStatut("confirmée");
+                System.out.println("Reservation confirmée pour le passager : "+ this.passager+"\n" + this.getNumeroReservation());
+            }else{
+                System.out.println("Pas de reservation correspendante pour "+ this.passager);
+            }
+        }
     }
 
-    public void annulerReservation(){
-        System.out.println("Reservation annulée pour le passager : " + this.getNumeroReservation());
+    public void annulerReservation(int numeroReservation){
+        for(Reservation reservation : passager.reservations){
+            if(reservation.getNumeroReservation() == numeroReservation){
+                passager.reservations.remove(reservation);
+                System.out.println("Reservation annulée pour le passager : "+ this.passager+"\n" + this.getNumeroReservation());
+            }else{
+                System.out.println("Pas de reservation correspendante pour "+ this.passager);
+            }
+        }
     }
 
-    public void modifierReservation(){
-        System.out.println("Reservation modifiée pour le passager : " + this.getNumeroReservation());
+    public void modifierReservation(int numeroReservation){
+        for(Reservation reservation : passager.reservations){
+            if(reservation.getNumeroReservation() == numeroReservation){
+                reservation.setStatut("modifiée");
+                System.out.println("Reservation modifiée pour le passager : "+ this.passager+"\n" + this.getNumeroReservation());
+            }else{
+                System.out.println("Pas de reservation correspendante pour "+ this.passager);
+            }
+        }
     }
     
+    public void addVol(int numeroVol){
+        for(Vol vol : vols){
+            if(vol.getNumeroVol() == numeroVol){
+                volsReserve.add(vol);
+            }
+        }
+    }
+
+    public void obtenirReservations(int numeroReservation){
+        for(Reservation reservation : passager.reservations){
+            if(reservation.getNumeroReservation() == numeroReservation){
+                System.out.println("Reservation : "+ reservation.getNumeroReservation() + ", Date : "+ reservation.getDateReservation() +
+                ", Statut : "+ reservation.getStatut() + ", Passager : "+ reservation.passager + ", Vols : "+ reservation.volsReserve);
+            }else{
+                System.out.println("Pas de reservation correspendante pour "+ this.passager);
+            }
+            
+        }
+    }
 }
